5c183ffb70e9e9732f42e78b850e64bc3c0d473a
Gustavo Camello <gucamello@gmail.com>
2020-12-01 19:22:39 +0000

    add ClubAPIController

---
+++ FootballInfo/Controllers/WebAPI/ClubsAPIController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FootballInfo.Models;
+
+namespace FootballInfo.Controllers.WebAPI
+{
+    public class ClubsAPIController : ApiController
+    {
+        private InfoContext db = new InfoContext();
+
+        // GET: api/ClubsAPI
+        public List<Club> GetClubs()
+        {
+            var clubs = db.Clubs.ToList();
+            return clubs;
+        }
+
+
+        // GET: api/ClubsAPI/5
+        [ResponseType(typeof(Club))]
+        public IHttpActionResult GetClub(int id)
+        {
+            Club club = db.Clubs.Find(id);
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(club);
+        }
+
+        // PUT: api/ClubsAPI/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutClub(int id, Club club)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != club.ClubId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(club).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClubExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ClubsAPI
+        [ResponseType(typeof(Club))]
+        public IHttpActionResult PostClub(Club club)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Clubs.Add(club);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = club.ClubId }, club);
+        }
+
+        // DELETE: api/ClubsAPI/5
+        [ResponseType(typeof(Club))]
+        public IHttpActionResult DeleteClub(int id)
+        {
+            Club club = db.Clubs.Find(id);
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            db.Clubs.Remove(club);
+            db.SaveChanges();
+
+            return Ok(club);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClubExists(int id)
+        {
+            return db.Clubs.Count(e => e.ClubId == id) > 0;
+        }
+    }
+}
