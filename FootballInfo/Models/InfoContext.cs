fdfdcadf64f0a5e10ce66e4195d21f20e2d53aed
Gustavo Camello <gucamello@gmail.com>
2020-12-01 18:40:49 +0000

    create infoContext

---
+++ FootballInfo/Models/InfoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FootballInfo.Models
+{
+    public class InfoContext:DbContext
+    {
+        public InfoContext() : base("InfoContext")
+        {
+
+        }
+
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Tournament> Tournaments { get; set; }
+    }
+}
